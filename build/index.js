b5dc9f74bc16f45e130c6c55ffbefb9668eff9e9
Michael Rockingham <mrockingham2@gmail.com>
2022-02-04 00:34:53 -0500

    test deploy

--- build/index.js
+++ build/index.js
@@ -6,6 +6,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const port = process.env.PORT || 5000;
 const app = (0, express_1.default)();
+app.get('/', (req, res) => {
+    res.status(200).json({ api: 'UP' });
+});
 app.listen(port, () => {
     console.log(`app is running on port ${port}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
 import express from 'express'
 const port = process.env.PORT || 5000
 const app = express()
+
+
+
+
+
+
+app.get('/', (req, res) => {
+  res.status(200).json({ api: 'UP' })
+})
 app.listen(port, () => {
   console.log(`app is running on port ${port}`)
 })
